b424d26474d0d599541b89cdb26e5131b47f6df6
Stephen <StephenLJosey@hotmail.com>
2017-07-12 09:14:52 -0700

    Updated upload to write to db.

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use App\Model\Entity\MediaItem;
+use Cake\ORM\TableRegistry;
 
 /* https://www.codexworld.com/handling-file-uploads-cakephp/  */
 
@@ -27,6 +29,14 @@ class UploadController extends AppController
                 $uploadFile = $uploadPath.$fileName;
                 if(move_uploaded_file($this->request->data['file']['tmp_name'],$uploadFile)){
                     $this->Flash->success(__('File has been uploaded and inserted successfully.'));
+                    $item = new MediaItem([
+                        'title' => $this->request->data['title'],
+                        'price' => $this->request->data['price'],
+                        'media_type' => 'image',
+                        'description' => $this->request->data['description'],
+                        'file_path' => $this->request->data['file']['name']
+                    ]);
+                    TableRegistry::get('Media_Items')->save($item);
                 }else{
 					$this->Flash->error(__('Unable to upload file, please try again.'));
 				}
--- src/Template/Upload/index.ctp
+++ src/Template/Upload/index.ctp
@@ -3,6 +3,9 @@
     <?= $this->Flash->render() ?>
     <div class="upload-frm">
         <?php echo $this->Form->create($uploadData, ['type' => 'file']); ?>
+        	<?php echo $this->Form->input('title', ['type' => 'text', 'class' => 'form-control']); ?>
+        	<?php echo $this->Form->input('price', ['type' => 'text', 'class' => 'form-control']); ?>
+        	<?php echo $this->Form->input('description', ['type' => 'text', 'class' => 'form-control']); ?>
             <?php echo $this->Form->input('file', ['type' => 'file', 'class' => 'form-control']); ?>
             <?php echo $this->Form->button(__('Upload File'), ['type'=>'submit', 'class' => 'form-controlbtn btn-default']); ?>
         <?php echo $this->Form->end(); ?>
